e600b325c5c448bfe8f99a3b8df58362dc9e99a1
mercyama <mercyezihe2@gmail.com>
2023-10-20 21:20:11 +0100

    updated client side

--- client/src/app/api/create-payment-intent/route.ts
+++ client/src/app/api/create-payment-intent/route.ts
@@ -1,49 +1,49 @@
-import Stripe from "stripe";
-import { NextResponse } from "next/server";
-import { CartProductType } from "@/app/productDetails/[productId]/page";
+// import Stripe from "stripe";
+// import { NextResponse } from "next/server";
+// import { CartProductType } from "@/app/productDetails/[productId]/page";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2023-10-16",
-});
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+//   apiVersion: "2023-10-16",
+// });
 
-const calculateOrderAmount = (items: CartProductType[]) => {
-  const totalPrice = items.reduce((accumulator, item) => {
-    const itemTotal = item.price * item.quantity;
-    return accumulator + itemTotal;
-  }, 0);
+// const calculateOrderAmount = (items: CartProductType[]) => {
+//   const totalPrice = items.reduce((accumulator, item) => {
+//     const itemTotal = item.price * item.quantity;
+//     return accumulator + itemTotal;
+//   }, 0);
 
-  return totalPrice;
-};
+//   return totalPrice;
+// };
 
-export async function POST(request: Request) {
-  const currentUser = await getCurrentUser();
+// export async function POST(request: Request) {
+//   const currentUser = await getCurrentUser();
 
-  if (!currentUser) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+//   if (!currentUser) {
+//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+//   }
 
-  const body = await request.json();
-  const { items, payment_intent_id } = body;
-  const total = calculateOrderAmount(items) * 100;
-  const orderData = {
-    user: { connect: { id: currentUser.id } },
-    amount: total,
-    currency: "usd",
-    status: "pending",
-    paymentIntentId: payment_intent_id,
-    products: items,
-  };
+//   const body = await request.json();
+//   const { items, payment_intent_id } = body;
+//   const total = calculateOrderAmount(items) * 100;
+//   const orderData = {
+//     user: { connect: { id: currentUser.id } },
+//     amount: total,
+//     currency: "usd",
+//     status: "pending",
+//     paymentIntentId: payment_intent_id,
+//     products: items,
+//   };
 
-  // Check if payment intent exists
-  if (payment_intent_id) {
-    // update the order
-  } else {
-    // create the intent
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total,
-      currency: "usd",
-      automatic_payment_methods: { enabled: true },
-    });
-    // create the order
-  }
-}
+//   // Check if payment intent exists
+//   if (payment_intent_id) {
+//     // update the order
+//   } else {
+//     // create the intent
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: total,
+//       currency: "usd",
+//       automatic_payment_methods: { enabled: true },
+//     });
+//     // create the order
+//   }
+// }
--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -1,63 +1,206 @@
 "use client";
-import { useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
+import Button from "@/components/Button";
+import { Heading } from "@/components/Heading";
+import { clearCart } from "@/redux/cartSlice";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-// const stripePromise = loadStripe(
-//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-// );
+export default function CheckoutPage() {
+  const cart = useSelector((state: any) => state.comb.cart.productList);
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-export default function Checkout() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+    paymentMethod: "Paystack",
+  });
+
+  const [orderSummary, setOrderSummary] = useState({
+    itemsPrice: 0,
+    shippingPrice: 0,
+    taxPrice: 0,
+    totalPrice: 0,
+  });
+
+  // calculate order summary based on cart contents
   useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-    if (query.get("success")) {
-      console.log("Order placed! You will receive an email confirmation.");
-    }
+    const itemsPrice = cart.reduce(
+      (total: any, item: any) => total + item.price * item.quantity,
+      0
+    );
+    const shippingPrice = 0;
+    const taxPrice = 0;
+    const totalPrice = itemsPrice + shippingPrice + taxPrice;
 
-    if (query.get("canceled")) {
-      console.log(
-        "Order canceled -- continue to shop around and checkout when you’re ready."
-      );
-    }
-  }, []);
+    setOrderSummary({ itemsPrice, shippingPrice, taxPrice, totalPrice });
+  }, [cart]);
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handlePlaceOrder = () => {
+    // clear cart after a succesful order creation
+    dispatch(clearCart());
+
+    // redirect to a confirmation page
+    router.push("/order-confirmation");
+  };
 
   return (
-    <form action="/api/checkout_sessions" method="POST">
-      <section>
-        <button type="submit" role="link">
-          Checkout
-        </button>
-      </section>
-      <style jsx>
-        {`
-          section {
-            background: #ffffff;
-            display: flex;
-            flex-direction: column;
-            width: 400px;
-            height: 112px;
-            border-radius: 6px;
-            justify-content: space-between;
-          }
-          button {
-            height: 36px;
-            background: #556cd6;
-            border-radius: 4px;
-            color: white;
-            border: 0;
-            font-weight: 600;
-            cursor: pointer;
-            transition: all 0.2s ease;
-            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-          }
-          button:hover {
-            opacity: 0.8;
-          }
-        `}
-      </style>
-    </form>
+    <div className="checkoutPage container max-w-2xl mx-auto p-4">
+      <Heading title="Checkout" center />
+      <div className="sm:flex flex-row space-x-10">
+        <form className=" w-6/12">
+          <label
+            className="block font-bold text-xl mb-4"
+            htmlFor="billingDetails"
+          >
+            Billing Details
+          </label>
+          {/* Fullname */}
+          <div className="mb-4">
+            <label className="block font-semibold" htmlFor="full name">
+              Full Name:
+            </label>
+            <input
+              className="w-full border rounded p-2"
+              type="text"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          {/* Email Address */}
+          <div className="mb-4">
+            <label className="block font-semibold" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className="w-full border rounded p-2"
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          {/* Address */}
+          <div className="mb-4">
+            <label className="block font-semibold" htmlFor="address">
+              Address:
+            </label>
+            <input
+              className="w-full border rounded p-2"
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          {/* City */}
+          <div className="mb-4">
+            <label className="block font-semibold" htmlFor="city">
+              City:
+            </label>
+            <input
+              className="w-full border rounded p-2"
+              type="text"
+              name="city"
+              value={formData.city}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          {/* Postal code */}
+          <div className="mb-4">
+            <label className="block font-semibold" htmlFor="postalCode">
+              Postal Code:
+            </label>
+            <input
+              className="w-full border rounded p-2"
+              type="number"
+              name="postalcode"
+              value={formData.postalCode}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          {/* Country */}
+          <div className="mb-4">
+            <label className="block font-semibold" htmlFor="country">
+              Country:
+            </label>
+            <input
+              className="w-full border rounded p-2"
+              type="text"
+              name="country"
+              value={formData.country}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          {/* Payment method */}
+          <div className="mb-4 mt-20">
+            <label className="block font-bold text-xl" htmlFor="paymentMethod">
+              Payment Method
+            </label>
+            <select
+              className="w-full border rounded p-2"
+              name="payment method"
+              value={formData.paymentMethod}
+              onChange={handleInputChange}
+            >
+              <option value="Paystack">Paystack</option>
+              <option value="Flutterwave">Flutterwave</option>
+              <option value="Paypal">Paypal</option>
+            </select>
+          </div>
+
+          {/* Place Order button */}
+          <div>
+            <Button label="Place Order" onClick={handlePlaceOrder} />
+          </div>
+        </form>
+
+        {/* Summarize the order */}
+        <div className="orderSummary mt-8 border p-4 w-5/12">
+          <Heading title="Order Summary" center />
+          <div className="mb-4 mt-2 p-2">
+            <div className="p-2">
+              Subtotal: ${orderSummary.itemsPrice.toFixed(2)}
+            </div>
+            <div className="p-2">
+              Shipping Price: ${orderSummary.shippingPrice.toFixed(2)}
+            </div>
+            <div className="p-2">
+              Tax Price: ${orderSummary.taxPrice.toFixed(2)}
+            </div>
+            <div className="p-2">
+              Total Price: ${orderSummary.totalPrice.toFixed(2)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
