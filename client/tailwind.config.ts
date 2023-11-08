72fa7b76d9668cfe0f03fa2db8b73f994d39b03f
mercyama <mercyezihe2@gmail.com>
2023-11-08 22:00:04 +0100

    updated client side

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,29 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
     "./node_modules/flowbite-react/**/*.js",
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
     fontFamily: {
       rem: ["REM", "sans-serif"],
       beginner: ["Edu SA Beginner", "cursive"],
       kanit: ["Kanit", "sans-serif"],
-      oswald: ["Oswald", "sans-serif"]
-    }
+      oswald: ["Oswald", "sans-serif"],
+    },
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
-export default config
+  plugins: [],
+};
+export default config;
